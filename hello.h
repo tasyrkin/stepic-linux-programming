96b64c28748576290b2085b02d31f6cb1e49fe8b
Timofey Asyrkin <tasyrkin@gmail.com>
2016-05-13 13:56:40 +0000

    C++ declaration conventions in header file using extern 'C'

--- hello.h
+++ hello.h
@@ -1,6 +1,10 @@
 #ifndef __HELLO__
 #define __HELLO__
 
+//extern "C" tells the C++ compiler to have "C" linkage, the compiler does not mangle the name
+#ifdef __cplusplus
+extern "C" 
+#endif
 void hello_message(const char* name);
 
 #endif //__HELLO__
